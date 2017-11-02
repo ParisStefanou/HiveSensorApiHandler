80b6b018cbc057a4aa3fb8778137bedf15e6e987
Paris Stefanou <gaspar093@hotmail.com>
2017-11-02 16:53:22 +0200

    Updated http connection test

--- src/main/java/upatras/hivesensorapihandler/utils/HttpRequestTest.java
+++ src/main/java/upatras/hivesensorapihandler/utils/HttpRequestTest.java
@@ -20,21 +20,26 @@ public class HttpRequestTest {
 
     public static void main(String[] args) {
 
-        ArrayList<Pair<String, String>> parameters=new ArrayList<>();
-        
+        authentication_test();
+        temperature_get_test();
+
+    }
+
+    public static void temperature_get_test() {
+
+        ArrayList<Pair<String, String>> parameters = new ArrayList<>();
+
         parameters.add(new Pair("start", "0"));
         parameters.add(new Pair("end", "10000000000"));
         parameters.add(new Pair("rate", "5"));
         parameters.add(new Pair("timeUnit", "MINUTES"));
         parameters.add(new Pair("operation", "AVG"));
-        
-        
+
         String response = getRequest("127.0.0.1", "/channels/temperature@CCpLNHBF-HkRl-1Ir8-IrzW-9GGRyUNW", 10000, parameters, null);
         System.out.println("received : \n\n" + JSONUtils.prettyprint(response));
-
     }
 
-    public static void main1(String[] args) {
+    public static void authentication_test() {
         JSONObject request = new JSONObject();
         JSONArray session = new JSONArray();
 
@@ -42,14 +47,14 @@ public class HttpRequestTest {
         data.put("username", "admin");
         data.put("password", "admin");
         data.put("caller", "WEB");
-        
+
         session.put(data);
         request.put("sessions", session);
-        
+
         System.out.println("sending : \n\n" + JSONUtils.prettyprint(request));
         String response = postRequest("127.0.0.1", "/auth/sessions", 10000, null, request.toString());
         System.out.println("received : \n\n" + JSONUtils.prettyprint(response));
-        
+
     }
 
 }
